8c4806313d5352206b43cfbec398b07c9854019f
Sayantan Sheet <sayantansheet@gmail.com>
2025-08-31 23:22:24 +0530

    env changes

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
-const server = {
-    dev: "http://localhost:8000",
-    prod:""
-}
+let IS_PROD = true;
+const server = IS_PROD ?
+            "https://videocallbackend-hy7y.onrender.com" :
+            "http://localhost:8000";
 
 export default server;
